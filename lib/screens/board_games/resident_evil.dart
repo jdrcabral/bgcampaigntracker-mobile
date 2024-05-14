125fd9c8aec0fcc74143181dd565cfd5c0c57caf
João Cabral <joao.denisr@gmail.com>
2024-05-14 17:27:03 -0300

    feat: fix changes

--- lib/screens/board_games/resident_evil.dart
+++ lib/screens/board_games/resident_evil.dart
@@ -9,10 +9,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class ResidentEvilCampaign extends ConsumerStatefulWidget {
   final Campaign campaign;
-<<<<<<< HEAD
 
-=======
->>>>>>> 4023ac1 (feat: fixed provider issues)
   const ResidentEvilCampaign({super.key, required this.campaign});
 
   @override
@@ -61,48 +58,48 @@ class _ResidentEvilCampaignState extends ConsumerState<ResidentEvilCampaign>
         children: [
           Center(
               child: Container(
-            padding: const EdgeInsets.all(30.0),
-            child: const Column(children: [
-              Text(
-                'Threat Level',
-                style: TextStyle(color: Colors.black, fontSize: 20),
-              ),
-              IncreaseDecreaseTextField(
-                stateKey: "threatLevel",
-              ),
-              Text(
-                'Notes',
-                style: TextStyle(color: Colors.black, fontSize: 20),
-              ),
-              TextField(
-                keyboardType: TextInputType.multiline,
-                maxLines: null,
-              ),
-            ]),
-          )),
+                padding: const EdgeInsets.all(30.0),
+                child: const Column(children: [
+                  Text(
+                    'Threat Level',
+                    style: TextStyle(color: Colors.black, fontSize: 20),
+                  ),
+                  IncreaseDecreaseTextField(
+                    stateKey: "threatLevel",
+                  ),
+                  Text(
+                    'Notes',
+                    style: TextStyle(color: Colors.black, fontSize: 20),
+                  ),
+                  TextField(
+                    keyboardType: TextInputType.multiline,
+                    maxLines: null,
+                  ),
+                ]),
+              )),
           Center(
               child: Container(
-            child: Flex(direction: Axis.vertical, children: [
-              Text('Active Characters',
-                  style: TextStyle(color: Colors.black)),
-              Expanded(
-                  flex: 1,
-                  child: ListView.builder(
-                      itemBuilder: (context, index) => Card(
+                child: Flex(direction: Axis.vertical, children: [
+                  Text('Active Characters',
+                      style: TextStyle(color: Colors.black)),
+                  Expanded(
+                      flex: 1,
+                      child: ListView.builder(
+                          itemBuilder: (context, index) => Card(
                             child: Row(
                               children: [],
                             ),
                           ))),
-              Text('Reserve Characters', style: TextStyle(color: Colors.black)),
-              Expanded(
-                flex: 2,
-                child: ListView.builder(
-                    itemCount: ref
+                  Text('Reserve Characters', style: TextStyle(color: Colors.black)),
+                  Expanded(
+                    flex: 2,
+                    child: ListView.builder(
+                        itemCount: ref
                             .watch(campaignSavedStatusProvider)
                             .savedState["reserve"]
                             ?.length ??
-                        0,
-                    itemBuilder: (context, index) => Card(
+                            0,
+                        itemBuilder: (context, index) => Card(
                           child: Container(
                               padding: const EdgeInsets.all(10.0),
                               child: Column(children: [
@@ -141,87 +138,87 @@ class _ResidentEvilCampaignState extends ConsumerState<ResidentEvilCampaign>
                                     IncreaseDecreaseTextField(
                                         stateKey: "threatLevel",
                                         mainAxisAlignment:
-                                            MainAxisAlignment.start),
+                                        MainAxisAlignment.start),
                                   ],
                                 ),
                               ])),
                         )),
-              ),
-            ]),
-          )),
+                  ),
+                ]),
+              )),
           Center(
               child: Column(
-            children: [
-              CardNavigator(
-                  cardTitle: 'Item Box',
-                  navigateTo: CardsController(
-                    title: 'Item Box',
-                    stateKey: 'itemBox',
-                    options: widget.campaign.savedState["selectOptions"]
+                children: [
+                  CardNavigator(
+                      cardTitle: 'Item Box',
+                      navigateTo: CardsController(
+                        title: 'Item Box',
+                        stateKey: 'itemBox',
+                        options: widget.campaign.savedState["selectOptions"]
                         ["items"],
-                    items: ref
-                        .watch(campaignSavedStatusProvider)
-                        .savedState["itemBox"],
-                  )),
-              CardNavigator(
-                cardTitle: 'Item A',
-                navigateTo: CardsController(
-                  title: 'Item A',
-                  stateKey: 'itemA',
-                  options: widget.campaign.savedState["selectOptions"]["items"],
-                  items: ref
-                      .watch(campaignSavedStatusProvider)
-                      .savedState["itemA"],
-                ),
-              ),
-              CardNavigator(
-                  cardTitle: 'Tension Deck',
-                  navigateTo: CardsController(
-                    title: 'Tension Deck',
-                    stateKey: 'tensionDeck',
-                    options: (widget.campaign.savedState["selectOptions"]
-                            ["tensionCards"] as List<dynamic>)
-                        .map((item) => "(${item['value']}) ${item['name']}")
-                        .toList(),
-                    items: ref
-                        .watch(campaignSavedStatusProvider)
-                        .savedState["tensionDeck"],
-                  )),
-              CardNavigator(
-                  cardTitle: 'Narrative Deck',
-                  navigateTo: CardsController(
-                    title: 'Narrative Deck',
-                    stateKey: 'playedNarratives',
-                    options: widget.campaign.savedState["selectOptions"]
+                        items: ref
+                            .watch(campaignSavedStatusProvider)
+                            .savedState["itemBox"],
+                      )),
+                  CardNavigator(
+                    cardTitle: 'Item A',
+                    navigateTo: CardsController(
+                      title: 'Item A',
+                      stateKey: 'itemA',
+                      options: widget.campaign.savedState["selectOptions"]["items"],
+                      items: ref
+                          .watch(campaignSavedStatusProvider)
+                          .savedState["itemA"],
+                    ),
+                  ),
+                  CardNavigator(
+                      cardTitle: 'Tension Deck',
+                      navigateTo: CardsController(
+                        title: 'Tension Deck',
+                        stateKey: 'tensionDeck',
+                        options: (widget.campaign.savedState["selectOptions"]
+                        ["tensionCards"] as List<dynamic>)
+                            .map((item) => "(${item['value']}) ${item['name']}")
+                            .toList(),
+                        items: ref
+                            .watch(campaignSavedStatusProvider)
+                            .savedState["tensionDeck"],
+                      )),
+                  CardNavigator(
+                      cardTitle: 'Narrative Deck',
+                      navigateTo: CardsController(
+                        title: 'Narrative Deck',
+                        stateKey: 'playedNarratives',
+                        options: widget.campaign.savedState["selectOptions"]
                         ["narratives"],
-                    items: ref
-                        .watch(campaignSavedStatusProvider)
-                        .savedState["playedNarratives"],
-                  )),
-              CardNavigator(
-                  cardTitle: 'Mission Deck',
-                  navigateTo: CardsController(
-                    title: 'Mission Deck',
-                    stateKey: 'playedMissions',
-                    options: widget.campaign.savedState["selectOptions"]
+                        items: ref
+                            .watch(campaignSavedStatusProvider)
+                            .savedState["playedNarratives"],
+                      )),
+                  CardNavigator(
+                      cardTitle: 'Mission Deck',
+                      navigateTo: CardsController(
+                        title: 'Mission Deck',
+                        stateKey: 'playedMissions',
+                        options: widget.campaign.savedState["selectOptions"]
                         ["missions"],
-                    items: ref
-                        .watch(campaignSavedStatusProvider)
-                        .savedState["playedMissions"],
-                  )),
-              CardNavigator(
-                  cardTitle: 'Encounter Deck',
-                  navigateTo: CardsController(
-                    title: 'Encounter Deck',
-                    stateKey: 'encounterDeck',
-                    options: widget.campaign.savedState["selectOptions"]
+                        items: ref
+                            .watch(campaignSavedStatusProvider)
+                            .savedState["playedMissions"],
+                      )),
+                  CardNavigator(
+                      cardTitle: 'Encounter Deck',
+                      navigateTo: CardsController(
+                        title: 'Encounter Deck',
+                        stateKey: 'encounterDeck',
+                        options: widget.campaign.savedState["selectOptions"]
                         ["encounters"],
-                    items: ref
-                        .watch(campaignSavedStatusProvider)
-                        .savedState["encounterDeck"],
-                  )),
-            ],
-          )),
+                        items: ref
+                            .watch(campaignSavedStatusProvider)
+                            .savedState["encounterDeck"],
+                      )),
+                ],
+              )),
           const Center(
               child: Text('Game map goes here',
                   style: TextStyle(color: Colors.black))),
