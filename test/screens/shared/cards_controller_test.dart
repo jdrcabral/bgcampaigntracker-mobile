7c8fc14b4b57599e15201d7167e8887bf6d428df
Joao Cabral <joao.cabral@complyadvantage.com>
2024-12-06 11:10:23 +0000

    feat: testing cards controller

--- test/screens/shared/cards_controller_test.dart
+++ test/screens/shared/cards_controller_test.dart
@@ -1,5 +1,6 @@
 import 'package:campaigntrackerflutter/models/campaign_status.dart';
 import 'package:campaigntrackerflutter/screens/shared/cards_controller.dart';
+import 'package:dropdown_search/dropdown_search.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -30,8 +31,47 @@ void main() {
       )
     );
 
-
     expect(find.text('first'), findsOneWidget);
     expect(find.text('second'), findsOneWidget);
+    
+    final floatingButton = find.byType(FloatingActionButton);
+    await tester.tap(floatingButton);
+    await tester.pumpAndSettle();
+    expect(find.byType(AlertDialog), findsOneWidget);
+
+    // Close with Cancel the dialog
+    final cancelButton = find.byType(TextButton).at(1);
+    await tester.tap(cancelButton);
+    await tester.pumpAndSettle();
+    expect(find.byType(AlertDialog), findsNothing);
+
+    // Reopen the dialog
+    await tester.tap(floatingButton);
+    await tester.pumpAndSettle();
+
+    // Select item and add
+    final dropdownSearch = find.byType(DropdownSearch<String>);
+    await tester.tap(dropdownSearch);
+    await tester.pumpAndSettle();
+
+    final item = find.text('options1');
+    await tester.tap(item);
+    await tester.pumpAndSettle();
+
+    final addButton = find.byType(TextButton).at(0);
+    await tester.tap(addButton);
+    await tester.pumpAndSettle();
+
+    expect(find.byType(AlertDialog), findsNothing);
+    expect(find.text('options1'), findsOneWidget);
+
+    // Delete the second item
+    final deleteButton = find.byType(IconButton).at(1);
+    await tester.tap(deleteButton);
+    await tester.pumpAndSettle();
+
+    expect(find.text('first'), findsOneWidget);
+    expect(find.text('second'), findsNothing);
+    expect(find.text('options1'), findsOneWidget);
   });
 }
